920eb80f597d72aba6b02ba93a58f71770bfa02f
Bharadwaj Sathya <bharadwajinamahanthi@gmail.com>
2026-09-20 16:09:49 +0530

    fix: remove ThemeProvider from main.tsx to simplify component structure

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
 import { PersistGate } from 'redux-persist/integration/react'
-import { ThemeProvider } from 'next-themes'
 import { BrowserRouter } from 'react-router-dom'
 import { store, persistor } from './store/store'
 import App from './App'
@@ -23,11 +22,9 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
       <PersistGate loading={<LoadingFallback />} persistor={persistor}>
-        <ThemeProvider attribute="class" defaultTheme="light" enableSystem={false}>
-          <BrowserRouter>
-            <App />
-          </BrowserRouter>
-        </ThemeProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
       </PersistGate>
     </Provider>
   </StrictMode>
